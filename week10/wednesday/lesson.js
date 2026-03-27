0fe5af19d1263fd6c1d9c8cba85152be252f03b1
Echo-007-sudo <bosspofii@gmail.com>
2026-03-27 10:36:53 +0100

    add untracked and modified files

---
+++ week10/wednesday/lesson.js
@@ -0,0 +1,11 @@
+//bracket notation
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(person["name"]); // Output: John
+console.log(person.name);
+console.log(`${person["name"]}`)
+
+//if the name of the key and the name of the variable of the value is the same, you can just use the the key 
---
+++ week10/wednesday/task.js
@@ -0,0 +1,35 @@
+/*
+ INDIVIDUAL TASK
+==================
+1. Create an initials generator program
+
+- Ask for full name
+- Output initials
+
+Example:
+Andrew Solex → A.S
+
+
+2. Create a word position finder
+
+- Ask for a sentence
+- Ask for a letter
+- Show the position of that letter
+
+3. Build a username generator
+
+- Ask for:
+-> First name
+-> Last name
+
+- Create a username like:
+andrewsolex_11
+
+(11 = total number of characters in first name and last name)
+
+
+To be submitted here: https://docs.google.com/forms/d/e/1FAIpQLSchcwvmfWZCFg5mhfcVwzvYpkVYaFN2pSFFhwvIDm0SyYP2Yg/viewform
+*/
+
+//1
+
